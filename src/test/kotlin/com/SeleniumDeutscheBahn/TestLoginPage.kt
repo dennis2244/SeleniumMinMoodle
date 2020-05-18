b54cae9276715f4339c44db59cc60a840c7da96c
lukas186 <lukas.von.thun@web.de>
2020-05-18 18:35:12 +0200

    Test

--- src/test/kotlin/com/SeleniumDeutscheBahn/TestLoginPage.kt
+++ src/test/kotlin/com/SeleniumDeutscheBahn/TestLoginPage.kt
@@ -1,6 +1,8 @@
 package com.SeleniumDeutscheBahn
 
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.testng.Assert
 import org.testng.annotations.AfterTest
@@ -28,5 +30,19 @@ class LoginTest {
         driver!!.get(url)
 
         Assert.assertEquals(driver!!.currentUrl, url, "URL did not match expected Value")
+
+        val usernameInputSelector = "#username"
+        val usernameInputBy = By.cssSelector(usernameInputSelector)
+        val usernameInputElement: WebElement = driver!!.findElement(usernameInputBy)
+
+        Assert.assertTrue(usernameInputElement.isDisplayed,"Element was not displyed")
+
+        val passwordInputSelector = "#password"
+        val passwordInputBy = By.cssSelector(passwordInputSelector)
+        val passwordInputElement: WebElement = driver!!.findElement(passwordInputBy)
+
+        Assert.assertTrue(passwordInputElement.isDisplayed,"Element was not displyed")
+
+
     }
 }
