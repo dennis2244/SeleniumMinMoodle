480589121f99f49731ffc28196eaba07f5a1c6a6
8thun <@8thun>
2020-06-30 17:27:08 +0200

    crossbrowser test login integriert + suite mit allen tests angelegt

--- src/test/kotlin/com/selenium_min_moodle/CrossBrowserTest.kt
+++ src/test/kotlin/com/selenium_min_moodle/CrossBrowserTest.kt
@@ -2,9 +2,12 @@ package com.selenium_min_moodle
 
 import com.selenium_min_moodle.data.TestData
 import com.selenium_min_moodle.pages.CourseViewPage
+import com.selenium_min_moodle.pages.LandingPage
+import com.selenium_min_moodle.pages.LoginPage
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.testng.Assert
 import org.testng.annotations.BeforeTest
 import org.testng.annotations.Parameters
 import org.testng.annotations.Test
@@ -33,6 +36,16 @@ class CrossBrowserTest {
         driver!!.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
         driver!!.get(TestData.url)
         driver!!.manage().window().maximize()
+        var loginPage = LoginPage(driver!!)
+        loginPage.login(TestData.username, TestData.password)
+        Assert.assertTrue(loginPage.isUserLoggedIn())
+        Assert.assertEquals(loginPage.meineKurse!!.text,(TestData.meineKurse))
+
+        var landingPage: LandingPage = LandingPage(driver!!)
+        landingPage.navigateToCourse()
+
+        var courseViewPage = CourseViewPage(driver!!)
+        Assert.assertTrue(courseViewPage.courseViewPageIsDisplayed())
 
     }
 
